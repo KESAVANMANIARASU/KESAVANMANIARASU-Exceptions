eabd30108a6a7199eea3533c234b7c3cee86ebb6
KESAVANMANIARASU <kesavanm0904@gmail.com>
2024-06-04 23:13:05 +0530

    commit

--- src/main/java/Lab.java
+++ src/main/java/Lab.java
@@ -1,3 +1,5 @@
+import java.io.IOException;
+
 public class Lab {
     /**
      * An exception is an unexpected event that occurs during our program.
@@ -18,7 +20,9 @@ public class Lab {
      * This also means that you should NOT write a try/catch block in this method, as the tests are expecting to have
      * a method thrown to it. A try/catch block would handle the exception within the method.
      */
-    public void mustThrow() {
+    public void mustThrow() throws IOException{
+        throw new IOException("hiii");
+        
 
     }
 }
